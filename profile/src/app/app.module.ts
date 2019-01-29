3bedd77c795f1221f0d5b88e01703ddbbc7e8420
Saurabh Mahajan <saurabh_mahajan29@yahoo.co.in>
2019-01-29 23:50:13 +0530

    about-me component, common scss created only

--- profile/src/app/app.module.ts
+++ profile/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule
