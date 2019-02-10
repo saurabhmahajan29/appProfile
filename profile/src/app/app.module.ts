5cccef2216212f84c24ae3f894d72e123526288e
Saurabh Mahajan <saurabh_mahajan29@yahoo.co.in>
2019-02-10 22:43:38 +0530

    strenths and testimonials carousel

--- profile/src/app/app.module.ts
+++ profile/src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppComponent } from './app.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { TimeLineComponent } from './time-line/time-line.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { StrengthsComponent } from './strengths/strengths.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutMeComponent,
     ExperienceComponent,
-    TimeLineComponent
+    TimeLineComponent,
+    TestimonialsComponent,
+    StrengthsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
