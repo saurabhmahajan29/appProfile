540b1e4e43768054e4836b3dbfeee4b110968c55
Saurabh Mahajan <saurabh_mahajan29@yahoo.co.in>
2019-02-13 21:05:58 +0530

    error corrected

--- profile/src/app/app.component.ts
+++ profile/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'profile';
-  skills: Array = [
+  skills: Array<any> = [
     {
       name: 'java',
       percentage: '80',
