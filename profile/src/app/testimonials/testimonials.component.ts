014b91db3f13336f4adfa154052a325850cfd494
Saurabh Mahajan <saurabh_mahajan29@yahoo.co.in>
2019-02-17 15:39:28 +0530

    testimonial from json

--- profile/src/app/testimonials/testimonials.component.ts
+++ profile/src/app/testimonials/testimonials.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-testimonials',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./testimonials.component.scss']
 })
 export class TestimonialsComponent implements OnInit {
-
+  @Input() testimonials;
   constructor() { }
 
   ngOnInit() {
